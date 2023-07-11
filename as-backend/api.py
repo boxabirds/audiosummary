e30a71e6367c6d8d69d2649c3c59d41a46b89dec
Julian Harris <julian.harris@gmail.com>
2023-07-11 13:56:18 +0100

    end to end WIP -- extract_audio is freezing process though

--- as-backend/api.py
+++ as-backend/api.py
@@ -9,6 +9,7 @@ from pydub.utils import mediainfo
 from pydub import AudioSegment
 import numpy as np
 import io
+from tqdm import tqdm
 
 
 app = Flask(__name__)
@@ -21,20 +22,49 @@ def get_sample_rate(file_path):
 # extract_segments_with_id takes a transcript and returns a list of segments with their ids
 def extract_segments_with_id(transcript):
     segments = []
-    for sentence in transcript:
+    for sentence in transcript["segments"]:
         segments.append({
-            'id': sentence.id,
-            'text': sentence.text
+            'id': sentence["id"],
+            'text': sentence["text"]
             # 'start': sentence.start,
             # 'end': sentence.end
         })
     return segments
 
-# parse_response takes an OpenAI response and returns a list of the ids of the top segments
+def estimate_num_tokens(text:str) -> int:
+    words = len(text.split())
+    return int(words * 100 / 65)  # OpenAI docs "You can think of tokens as pieces of words, where 1,000 tokens is about 750 words." but we found that this estimate is on the optimistic side
+
+
+# convert_openai_response_to_int_array takes an OpenAI response and returns a list of the ids of the top segments
 # the output is simply a text string e.g. [2, 15, 17, 18, 24, 27, 28, 31, 32, 35, 41, 43, 54, 66, 67, 73, 74, 76]
-def parse_response(response):
+# sample OpenAI response
+# OpenAI response: '{
+#   "id": "chatcmpl-7b6XjdWokuIeWCVyGUCUVSbAuzlGM",
+#   "object": "chat.completion",
+#   "created": 1689077643,
+#   "model": "gpt-4-0613",
+#   "choices": [
+#     {
+#       "index": 0,
+#       "message": {
+#         "role": "assistant",
+#         "content": "[3, 4, 11, 14, 21, 24, 25, 30, 35, 37, 40, 42, 44, 45, 59, 62, 64, 68, 70, 79, 81, 82, 84, 85, 86, 88, 90]"
+#       },
+#       "finish_reason": "stop"
+#     }
+#   ],
+#   "usage": {
+#     "prompt_tokens": 2964,
+#     "completion_tokens": 81,
+#     "total_tokens": 3045
+#   }
+# }'
+# I think this will change shortly to use ChatML which is nice cos parsing this stuff is error-prone and slow
+def convert_openai_response_to_int_array(response):
     # convert the response to a list of integers
-    response = response.choices[0].text
+    print( f"OpenAI response: '{response}'")
+    response = response.choices[0].message.content
     response = response.replace("[", "")
     response = response.replace("]", "")
     response = response.replace(" ", "")
@@ -56,12 +86,14 @@ def extract_audio(segment, audio_data, sample_rate=16000):
     Returns:
     AudioSegment: The extracted audio segment.
     """
+    print(f"Extracting audio segment: {segment}")
     # Convert the audio data to a byte stream
     audio_byte_stream = io.BytesIO()
     audio_as_int16 = np.int16(audio_data * 32767).tobytes()
     audio_byte_stream.write(audio_as_int16)
     audio_byte_stream.seek(0)
 
+    print(f"Audio byte stream: {audio_byte_stream}")
     # Load the audio data as an AudioSegment
     audio = AudioSegment.from_raw(audio_byte_stream, sample_width=2, channels=1, frame_rate=sample_rate)
 
@@ -75,7 +107,7 @@ def extract_audio(segment, audio_data, sample_rate=16000):
     return segment_audio
 
 
-from pydub import AudioSegment
+
 
 def stitch_audio(audio_segments, fade_in, fade_out, silence):
     """
@@ -107,9 +139,23 @@ def stitch_audio(audio_segments, fade_in, fade_out, silence):
     return stitched_audio
 
 
+TRANSCRIPT_PREFIX = ".json"
+SEGMENTS_PREFIX = ".segments.json"
+MAX_TOKENS = 8192
+PROMPT_BASE = "below is a list of sentence segments, prefixed by their segment id. please perform an extractive summary of no more than 20% of these sentences: specifically assess the text of all the segments and return the ids only of the segments that you deem to be the most important and as such meet the requirements of the top 20%.\n\n"
+PROMPT_BASE_ESTD_NUM_TOKENS = estimate_num_tokens(PROMPT_BASE)
 
-@app.route('/process_audio', methods=['POST'])
+# function to send one request to OpenAI
+def send_openai_request(segment_batch):
+    prompt = PROMPT_BASE + json.dumps(segment_batch)
+    estd_num_prompt_tokens = estimate_num_tokens(prompt)
+    response = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "system", "content": prompt}], max_tokens=MAX_TOKENS-estd_num_prompt_tokens)
+    return response
+
+
+@app.route('/process_audio', methods=['POST', 'OPTIONS'])
 def process_audio():
+
     CACHE_DIR = "cache/"
     os.makedirs(CACHE_DIR, exist_ok=True)
     
@@ -121,57 +167,94 @@ def process_audio():
         return jsonify({'error': 'No selected file'}), 400
     
     # save the mp3 to the cache directory
+    audio = file.read()
     with open(CACHE_DIR + file.filename, 'wb') as f:
-        f.write(file.read())
+        f.write(audio)
 
-    result = None
-    # we cache the transcript mostly for development purposes -- depending on model size and audio length, it can take 10 minutes or as long as 2 hours to transcribe
-    # if the corresponding cache file exists, use that instead of transcribing again
-    if os.path.exists(CACHE_DIR + file.filename + ".json" ):
-        # load the cached mp3 file
-        with open(CACHE_DIR + file.filename, 'rb') as f:
-            audio = f.read()
+    transcript = None
 
-        with open(CACHE_DIR + file.filename + ".json") as f:
-            result = json.load(f)
-
-        with open(CACHE_DIR + file.filename + "segments.json") as f:
-            segments = json.load(f)
-
+    # transcript is cached
+    if os.path.exists(CACHE_DIR + file.filename + TRANSCRIPT_PREFIX ):
+        with open(CACHE_DIR + file.filename + TRANSCRIPT_PREFIX) as f:
+            transcript = json.load(f)
+            
+    # transcript is not cached
     else:
+        print("Cache not found -- transcribing audio file {}".format(file.filename))
         # Load the audio and model
         audio = whisper.load_audio(file.filename)
         model = whisper.load_model("tiny", device="cpu") # use "medium" for better results but 10x slower
 
         # Transcribe the audio
-        result = whisper.transcribe_timestamped(model, audio, language="en", vad=True, beam_size=5, best_of=5, temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0))
+        transcript = whisper.transcribe_timestamped(model, audio, language="en", vad=True, beam_size=5, best_of=5, temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0))
 
         # cache result in a file with the same name as the uploaded file
-        with open(CACHE_DIR + file.filename + ".json", 'w') as f:
-            json.dump(result, f)
+        with open(CACHE_DIR + file.filename + TRANSCRIPT_PREFIX, 'w') as f:
+            json.dump(transcript, f)
 
-        segments = extract_segments_with_id(result)
-        with open(file.filename + ".segments.json", 'w') as f:
+    # segments are cached
+    if os.path.exists(CACHE_DIR + file.filename + SEGMENTS_PREFIX ):
+        print("Cache found -- loading segments from file")
+        with open(CACHE_DIR + file.filename + SEGMENTS_PREFIX) as f:
+            segments = json.load(f)
+
+    # segments are not cached
+    else:
+        print("Cache not found -- extracting segments from transcript")
+        segments = extract_segments_with_id(transcript)
+        with open(CACHE_DIR + file.filename + SEGMENTS_PREFIX, 'w') as f:
             json.dump(segments, f)
 
-    # Step 2: Select the top segments
-    prompt = "below is a list of sentence segments, prefixed by their segment id. please perform an extractive summary of no more than 20% of these sentences: specifically assess the text of all the segments and return the ids only of the segments that you deem to be the most important and as such meet the requirements of the top 20%.\n\n" + json.dumps(segments)
-    response = openai.ChatCompletion.create(model="gpt4", messages=[{"role": "system", "content": prompt}], max_tokens=32000)
+    # Step 2: Select the top segments in batches to overcome OpenAI's limit
+    responses = []
+    segment_batch = []
+    total_tokens = 0
 
-    print("openai.ChatCompletion response: {}".format(response))
+    for segment in tqdm(segments):
+        num_segment_tokens = estimate_num_tokens(json.dumps(segment, indent=4))
 
-    top_ids = parse_response(response)
-    top_segments = [segment for segment in segments if segment.id in top_ids]
+        # we have reached the max number of tokens, so send the batch to OpenAI
+        if (total_tokens + num_segment_tokens + PROMPT_BASE_ESTD_NUM_TOKENS) > MAX_TOKENS:
+            response = send_openai_request(segment_batch)
+            responses.append(response)
+
+            # new batch
+            segment_batch = [segment]
+
+            total_tokens = num_segment_tokens
+
+        # we have not reached the max number of tokens, so add the segment to the batch
+        else:
+            segment_batch.append(segment)
+            total_tokens += num_segment_tokens
+
+    # send the final batch to OpenAI
+    if len(segment_batch) > 0:
+        print(f"Sending final batch to OpenAI: {len(segment_batch)} segments, {total_tokens} tokens")
+        response = send_openai_request(segment_batch)
+        responses.append(response)
+
+    top_ids = [convert_openai_response_to_int_array(r) for r in responses]
+    top_ids = [item for sublist in top_ids for item in sublist]
+
+    # the segment id is a sequence so to ensure we preserve the order we sort the segments by id
+    top_segments = sorted([segment for segment in segments if segment['id'] in top_ids], key=lambda x:x['id'])
+    print( f"Top segments: {top_segments}")
 
     # Step 3: Extract the corresponding audio
     sample_rate = get_sample_rate(file.filename)
     audio_segments = [extract_audio(segment, audio, sample_rate) for segment in top_segments]
+    # dump audio segments to file for debugging
+    print(f"Dumping {len(audio_segments)} audio segments to files")
+    for i, audio_segment in enumerate(audio_segments):
+        audio_segment.export(f"segment_{i}.mp3", format="mp3")
 
     # Step 4: Stitch the audio together
     summary_audio = stitch_audio(audio_segments, fade_in=0.3, fade_out=0.3, silence=1)
 
     # Save the summary audio to a file
-    summary_audio.export("summary.mp3", format="mp3")
+    print(f"Saving summary audio to {file.filename}-summary.mp3")
+    summary_audio.export(f"{file.filename}-summary.mp3", format="mp3")
 
 
     response = {
